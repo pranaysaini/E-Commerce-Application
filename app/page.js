95a757a7898692c0df897f5a6bf1a92932e86ad8
Pranay Saini <sainipranay36@gmail.com>
2025-01-25 15:14:39 +0530

    added css for products

--- app/page.js
+++ app/page.js
@@ -48,12 +48,7 @@ export default function ProductList() {
           </div>
       </Link>
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-          {products.map(product => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
-
+      
     </>
   );
 }
--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -41,34 +41,38 @@ export default function ProductPage() {
  
 
   return (
-    <div className="p-8">
+    <div className="p-8 flex justify-center gap-x-20 ">
       <img
         src={product.image}
         alt={product.name}
         className="w-full max-w-md h-auto object-cover"
       />
-      <h1 className="text-2xl font-bold mt-4">{product.name}</h1>
-      <p className="text-gray-700 mt-2">₹{product.price}</p>
-      <p className="mt-4 text-sm text-gray-500">{product.description}</p>
+      
+      <div className="flex flex-col">
 
-      <div className="flex items-center mb-4">
-        <label htmlFor="quantity" className="mr-2">Qty:</label>
-        <input
-          id="quantity"
-          type="number"
-          min="1"
-          defaultValue="1"
-          className="border w-12 text-center"
-        />
+          <h1 className="text-2xl font-bold mt-4">{product.name}</h1>
+          <p className="mt-2 text-2xl">₹{product.price}</p>
+          <p className="mt-4 text-sm text-gray-500">{product.description}</p>
+
+          <div className="flex items-center mb-4 mt-10">
+            <label htmlFor="quantity" className="mr-2 text-lg font-serif">Quantity</label>
+            <input
+              id="quantity"
+              type="number"
+              min="1"
+              defaultValue="1"
+              className="border w-12 text-center text-xl"
+            />
+          </div>
+
+          <button
+            className="bg-black hover:bg-white hover:text-black hover:border-solid hover:border-2 hover:border-black text-white border-double font-bold px-4 py-2 mt-1 rounded"
+            // onClick={() => addToCart(product, parseInt(document.getElementById('quantity').value))}
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </button>
       </div>
-
-      <button
-        className="bg-blue-500 text-white px-4 py-2 mt-4 rounded"
-        // onClick={() => addToCart(product, parseInt(document.getElementById('quantity').value))}
-        onClick={handleAddToCart}
-      >
-        Add to Cart
-      </button>
 
       
     </div>
