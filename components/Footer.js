c1de42c6ecf42ee717619ab25f16dfaa39c836a7
Pranay Saini <sainipranay36@gmail.com>
2025-01-25 15:54:36 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <>
             <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://grandbazar.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://grandbazar.com/" className="hover:underline">Grand Bazar</a>. All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
