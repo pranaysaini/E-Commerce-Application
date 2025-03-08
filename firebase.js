208dcab2335d2807c892eb1f305e58ba2cd0f49d
Pranay Saini <sainipranay36@gmail.com>
2025-03-08 11:32:02 +0530

    added more features

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyD7u-DIiOP-TlftkMAe_7bls9HZPa0Arxw" ,
+    authDomain: "e-commerce-application-31a8b.firebaseapp.com" ,
+    projectId: "e-commerce-application-31a8b" ,
+    storageBucket: "e-commerce-application-31a8b.firebasestorage.app" ,
+    messagingSenderId: "887382586758" ,
+    appId: "1:887382586758:web:d8dd1e53d01e15b08de807" ,
+    measurementId: "G-5Z97VPQCZX"
   };
 
   // const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
